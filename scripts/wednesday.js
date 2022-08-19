418a2732ab265b2133f09a771ebf00d097878a3c
kelvin <kelvinperrie@gmail.com>
2022-08-19 15:46:07 +1200

    mucking round with cookies

--- scripts/wednesday.js
+++ scripts/wednesday.js
@@ -76,19 +76,50 @@ function HandleFrogClick() {
 
 // we don't have a dark and light mode, just a new and classic frog modes
 function HandleToggleTypeClick() {
+  let type = "new";
     if($("#togBtn").is(":checked")) {
         $(".parent").removeClass("classic-frog").addClass("new-frog");
     } else {
         $(".parent").removeClass("new-frog").addClass("classic-frog");
+        type = "classic";
     }
+    console.log("setting cookie value to " + type)
+    setCookie("frogType", type);
 }
 
 
 $(document).ready(function() {
   $(".frog-image").on("click", HandleFrogClick);
 
-    $("#togBtn").on("click", HandleToggleTypeClick);
+  $("#togBtn").on("click", HandleToggleTypeClick);
+
+  var lastFrogType = getCookie("frogType");
+  console.log("got cookie value of " + lastFrogType)
+  if(lastFrogType && lastFrogType == "classic") {
+    console.log("doing click on toggle")
+    $('#togBtn').trigger('click');
+  }
 
   // initially, just run the animation
   setTimeout(DoAnimation, 2000);
 });
+
+function setCookie(name,value,days) {
+  var expires = "";
+  if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days*24*60*60*1000));
+      expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
